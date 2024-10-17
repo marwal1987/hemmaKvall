961bc711251be8ce33024894499d289cfcb927db
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-10-17 11:23:17 +0200

    Removed, in my opinion, redundant handleSearch in Searchbar. Replaced it with arrow in onClick

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,23 +2,19 @@ import { useState } from "react";
 
 /**
  * SearchBar Komponent
- * 
+ *
  * En enkel sökfältkomponent som tillåter användaren att söka efter en filmtitel.
  * När användaren skriver in en sökterm och klickar på "Sök", anropas en callback-funktion via `onSearch`-prop.
- * 
+ *
  * Props:
  * - onSearch: Callback-funktion som anropas när användaren klickar på "Sök"-knappen, skickar söktermen som argument
- * 
+ *
  * State:
  * - Hanterar sökordet
  */
 
 const SearchBar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearch = () => {
-      onSearch(searchTerm);
-  };
 
   return (
     <div className="flex p-6 justify-center items-center gap-6">
@@ -32,7 +28,7 @@ const SearchBar = ({ onSearch }) => {
         className="border p-2 w-fit"
       />
       <button
-        onClick={handleSearch}
+        onClick={() => onSearch(searchTerm)}
         disabled={!searchTerm.trim()} // Inaktivera om ingen sökterm finns
         className={`bg-gray-800 text-white px-4 py-2 border-2 border-gray-light 
         hover:border-gray-dark hover:bg-gray-light hover:text-gray-dark 
